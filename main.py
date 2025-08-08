56e3efcfe0882b067c5227e27ca45cc3d5116f3d
Drew Goldsberry <connect@drewgoldsberry.com>
2025-08-08 11:56:36 -0700

    updated model with latest, added packages used, add comment in main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from agent_player import AgentPlayer
 from transformer_agent import TransformerAgent
 from player import Player
 if __name__ == "__main__":
-   # ui = BlackjackUI()
-   # ui.run()
-    #agent = TransformerAgent.load("./models/blackjack_agent_ep_dumb.pt")
-    agent = TransformerAgent.load("./models/blackjack_agent_ep.pt")
-    player = AgentPlayer("Jimmy", agent)
-    #player= Player("Human")
-    ui = BlackjackUI(agent=player, is_agent=True)
-    ui.run()
+   #defaults to TransormerAgent being used to play the game
+   agent = TransformerAgent.load("./models/blackjack_agent_ep.pt")
+   player = AgentPlayer("Jimmy", agent)
+   ui = BlackjackUI(agent=player, is_agent=True)
+
+   #uncomment to play as a human
+   #player= Player("Human")
+   # ui = BlackjackUI(agent=player, is_agent=False)
+   ui.run()
